a4fbf0b26383d4900e84c0ccd8fd51644b137422
zitz59 <dondongudon@gmail.com>
2025-07-03 19:33:13 +0200

    change styles for burgerIconMenu.svg

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -2,36 +2,44 @@
 
 import styles from "./Header.module.scss"
 import Image from "next/image"
-import {ForwardedRef, forwardRef} from "react"
+import {ForwardedRef, forwardRef, useState} from "react"
 import Navbar from "@/app/components/NavBar/Navbar";
 
 type PropsType = {
-    handleOpen: () => void
+	handleOpen: () => void
 }
 
 export const Header = forwardRef<HTMLDivElement, PropsType>(
-    ({ handleOpen }, ref: ForwardedRef<HTMLDivElement>) => {
-        return (
-            <div ref={ref} className={styles.header}>
-                <div className={styles.logoContainer}>
-                    <img src="/logo/label_logo.jpg"  alt="logo" width={100} height={100} style={{objectFit:"cover",borderRadius:"8px"}} />
-                </div>
-                <div className={styles.navWrapper}>
-                    <Navbar />
-                </div>
-                <Image
-                    width={30}
-                    height={30}
-                    src="/icons/burgerIconMenu.svg"
-                    unoptimized
-                    id="burger-menu"
-                    alt="open-menu"
-                    className={styles.burgerMenuIcon}
-                    onClick={handleOpen}
-                />
-            </div>
-        )
-    }
+	({handleOpen}, ref: ForwardedRef<HTMLDivElement>) => {
+		const [open, setOpen] = useState(false)
+
+		const onClickHandler = () => {
+			setOpen(prev => !prev)
+			handleOpen()
+		}
+
+		return (
+			<div ref={ref} className={styles.header}>
+				<div className={styles.logoContainer}>
+					<img src="/logo/label_logo.jpg" alt="logo" width={100} height={100}
+						 style={{objectFit: "cover", borderRadius: "8px"}}/>
+				</div>
+				<div className={styles.navWrapper}>
+					<Navbar/>
+				</div>
+				<Image
+					width={40}
+					height={40}
+					src="/icons/hamburgerMenu.png"
+					unoptimized
+					id="burger-menu"
+					alt="open-menu"
+					className={`${styles.burgerMenuIcon} ${open ? styles.open : ''}`}
+					onClick={onClickHandler}
+				/>
+			</div>
+		)
+	}
 )
 Header.displayName = 'Header'
 
