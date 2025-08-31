967f08ded52834e49c583138e03b0bae07d712c0
zitz59 <dondongudon@gmail.com>
2025-08-31 21:28:13 +0200

    add data

---
+++ lib/releases/mockData.ts
@@ -0,0 +1,24 @@
+import type {Release} from "@/types/release";
+
+export const RELEASES: Release[] = [
+	{
+		slug: "hail-bob-001",
+		artist: "Various Artists",
+		title: "Chemical House For Sad Earth Residents",
+		year: 2025,
+		catalogNumber:"HBR001",
+		cover: "/images/releases/hail-bob-001.jpg",
+		description: "This is the anti-war compilation.\n" +
+			"\n" +
+			"Originally mastered in high quality — only 320 kbps survived.\n" +
+			"\n" +
+			"Design by mkbr_desecrator\n" +
+			"Curated by Саша Шульгин\n" +
+			"\n" +
+			"HBR001",
+		links: {
+			bandcamp: "https://hailbobrecords.bandcamp.com/album/chemical-house-for-sad-earth-residents",
+		}
+	}
+]
+
