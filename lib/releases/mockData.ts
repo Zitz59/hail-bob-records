8507f305f0063aaf416d3710bd8c69c084fd0c52
zitz59 <dondongudon@gmail.com>
2025-11-13 02:33:21 +0100

    add data for new release

--- lib/releases/mockData.ts
+++ lib/releases/mockData.ts
@@ -1,12 +1,31 @@
 import type {Release} from "@/types/release";
 
-export const RELEASES: Release[] = [
+export const RELEASES: Release[] = [{
+	slug: "hail-bob-000",
+	artist: "Various Artists",
+	title: "\n" +
+		"Hail Bob From Outer Space",
+	year: 2025,
+	catalogNumber: "HBR000",
+	cover: "/images/releases/hail-bob-000.jpg",
+	description: "Here’s the compilation that started it all for Hail Bob Records.\n" +
+		"\n" +
+		"Chronologically the first, but released second. Schizophrenia makes an appearance.\n" +
+		"\n" +
+		"Tracks come from both established names and new artists, covering\n" +
+		"Brussels, Zagreb, Rostov-on-Don, and other European locations.\n" +
+		"\n" +
+		"HBR000",
+	links: {
+		bandcamp: "https://hailbobrecords.bandcamp.com/album/hail-bob-from-outer-space",
+	}
+},
 	{
 		slug: "hail-bob-001",
 		artist: "Various Artists",
 		title: "Chemical House For Sad Earth Residents",
 		year: 2025,
-		catalogNumber:"HBR001",
+		catalogNumber: "HBR001",
 		cover: "/images/releases/hail-bob-001.jpg",
 		description: "This is the anti-war compilation.\n" +
 			"\n" +
