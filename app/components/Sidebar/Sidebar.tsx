ce156cf9651fa66c4719dd3c1b677db83d8e7cbc
Zitz59 <dondongudon@gmail.com>
2025-11-22 23:43:32 +0100

    fixed bugs with sidebar and tablets view

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -5,45 +5,41 @@ import s from './Sidebar.module.scss'
 import Image from "next/image";
 
 type PropsType = {
-	open: boolean
-	action: () => void
+    open: boolean
+    action: () => void
 }
 
-export const Sidebar: FC<PropsType> = ({ open, action }) => {
-	return (
-		<>
-			{/* затемнение */}
-			{open && (
-				<div className={s.background} onClick={action} />
-			)}
+export const Sidebar: FC<PropsType> = ({open, action}) => {
+    return (
+        <>
+            {/* затемнение */}
+            {open && (
+                <div className={s.background} onClick={action}/>
+            )}
 
-			<aside
-				className={s.sidebar}
-				data-open={open}
-				aria-hidden={!open}
-			>
-				<button className={s.close} onClick={action}>
-					<Image
-						width={55}
-						height={40}
-						src="/icons/close_icon.png"
-						alt="close sidebar"
-					/>
-				</button>
+            <aside className={`${s.sidebar} ${open ? s.open : ""}`}>
+                <button className={s.close} onClick={action}>
+                    <Image
+                        width={55}
+                        height={40}
+                        src="/icons/close_icon.png"
+                        alt="close sidebar"
+                    />
+                </button>
 
-				<nav className={s.nav}>
-					{navLinks.map(({ id, label, href }) => (
-						<NavLink
-							key={id}
-							id={id}
-							href={href}
-							onClick={action}
-						>
-							{label}
-						</NavLink>
-					))}
-				</nav>
-			</aside>
-		</>
-	);
+                <nav className={s.nav}>
+                    {navLinks.map(({id, label, href}) => (
+                        <NavLink
+                            key={id}
+                            id={id}
+                            href={href}
+                            onClick={action}
+                        >
+                            {label}
+                        </NavLink>
+                    ))}
+                </nav>
+            </aside>
+        </>
+    );
 };
