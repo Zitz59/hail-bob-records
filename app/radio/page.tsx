6a19560828ca9d30255bb340b1e341e57b7a3381
Zitz59 <dondongudon@gmail.com>
2026-01-19 21:45:41 +0100

    add Radio page

---
+++ app/radio/page.tsx
@@ -0,0 +1,8 @@
+import RadioPlayer from "@/app/components/RadioPlayer/RadioPlayer";
+import React from "react";
+
+export default function Radio(){
+    return (
+        <div><RadioPlayer/></div>
+    )
+}
