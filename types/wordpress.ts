714f04e3326d416c05739b68edf4a51be34d278e
Zitz59 <dondongudon@gmail.com>
2026-01-25 23:10:18 +0100

    add correct types for _embedded

--- types/wordpress.ts
+++ types/wordpress.ts
@@ -1,9 +1,24 @@
 export type WPPost = {
-	id: number
-	slug: string
-	date: string
-	title: { rendered: string }
-	excerpt: { rendered: string }
-	content: { rendered: string }
-	_embedded: any
+    id: number
+    slug: string
+    date: string
+    title: { rendered: string }
+    excerpt: { rendered: string }
+    content: { rendered: string }
+    _embedded?: {
+        'wp:featuredmedia'?: Array<{
+            source_url: string;
+            media_details?: {
+                sizes: {
+                    full: { source_url: string };
+                    large?: { source_url: string };
+                }
+            }
+        }>;
+    };
+    acf?: {
+        artist?: string;
+        year?: string;
+        catalog_number?: string;
+    };
 }
