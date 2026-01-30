49e68bbe2ba6b420b3458a818fa88be828b3577e
Zitz59 <dondongudon@gmail.com>
2026-01-30 01:15:48 +0100

    add instagram link

--- app/components/SocialLinks/SocialLinks.tsx
+++ app/components/SocialLinks/SocialLinks.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function SocialLinks() {
 	return (
 		<div className={styles.socials}>
-			<Link href="https://instagram.com/yourusername" target="_blank" rel="noopener noreferrer">
+			<Link href="https://www.instagram.com/hailbobrecords/" target="_blank" rel="noopener noreferrer">
 				<Image src="/icons/icons8-instagram.svg" alt="Instagram" width={44} height={44} />
 			</Link>
 			<Link href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer">
