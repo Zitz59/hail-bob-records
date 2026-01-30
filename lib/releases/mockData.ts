252b9de568d434fb7e495fab5fd6db40dc3874c8
Zitz59 <dondongudon@gmail.com>
2026-01-30 11:53:50 +0100

    issued types error

--- lib/releases/mockData.ts
+++ lib/releases/mockData.ts
@@ -1,43 +1,43 @@
 import type {Release} from "@/types/release";
 
 export const RELEASES: Release[] = [{
-	slug: "hail-bob-000",
-	artist: "Various Artists",
-	title: "\n" +
-		"Hail Bob From Outer Space",
-	year: 2025,
-	catalogNumber: "HBR000",
-	cover: "/images/releases/hail-bob-000.jpg",
-	description: "Here’s the compilation that started it all for Hail Bob Records.\n" +
-		"\n" +
-		"Chronologically the first, but released second. Schizophrenia makes an appearance.\n" +
-		"\n" +
-		"Tracks come from both established names and new artists, covering\n" +
-		"Brussels, Zagreb, Rostov-on-Don, and other European locations.\n" +
-		"\n" +
-		"HBR000",
-	links: {
-		bandcamp: "https://hailbobrecords.bandcamp.com/album/hail-bob-from-outer-space",
-	}
+    slug: "hail-bob-000",
+    artist: "Various Artists",
+    title: "\n" +
+        "Hail Bob From Outer Space",
+    year: "2025",
+    catalogNumber: "HBR000",
+    cover: "/images/releases/hail-bob-000.jpg",
+    description: "Here’s the compilation that started it all for Hail Bob Records.\n" +
+        "\n" +
+        "Chronologically the first, but released second. Schizophrenia makes an appearance.\n" +
+        "\n" +
+        "Tracks come from both established names and new artists, covering\n" +
+        "Brussels, Zagreb, Rostov-on-Don, and other European locations.\n" +
+        "\n" +
+        "HBR000",
+    links: {
+        bandcamp: "https://hailbobrecords.bandcamp.com/album/hail-bob-from-outer-space",
+    }
 },
-	{
-		slug: "hail-bob-001",
-		artist: "Various Artists",
-		title: "Chemical House For Sad Earth Residents",
-		year: 2025,
-		catalogNumber: "HBR001",
-		cover: "/images/releases/hail-bob-001.jpg",
-		description: "This is the anti-war compilation.\n" +
-			"\n" +
-			"Originally mastered in high quality — only 320 kbps survived.\n" +
-			"\n" +
-			"Design by mkbr_desecrator\n" +
-			"Curated by Саша Шульгин\n" +
-			"\n" +
-			"HBR001",
-		links: {
-			bandcamp: "https://hailbobrecords.bandcamp.com/album/chemical-house-for-sad-earth-residents",
-		}
-	}
+    {
+        slug: "hail-bob-001",
+        artist: "Various Artists",
+        title: "Chemical House For Sad Earth Residents",
+        year: "2025",
+        catalogNumber: "HBR001",
+        cover: "/images/releases/hail-bob-001.jpg",
+        description: "This is the anti-war compilation.\n" +
+            "\n" +
+            "Originally mastered in high quality — only 320 kbps survived.\n" +
+            "\n" +
+            "Design by mkbr_desecrator\n" +
+            "Curated by Саша Шульгин\n" +
+            "\n" +
+            "HBR001",
+        links: {
+            bandcamp: "https://hailbobrecords.bandcamp.com/album/chemical-house-for-sad-earth-residents",
+        }
+    }
 ]
 
