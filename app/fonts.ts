e55b35f6a23fc04ea982fd30004d63b73de7de8b
Zitz59 <dondongudon@gmail.com>
2026-01-31 19:14:41 +0100

    added and changed fonts

--- app/fonts.ts
+++ app/fonts.ts
@@ -3,14 +3,14 @@ import localFont from "next/font/local";
 export const hailBobFont = localFont({
     src: [
         {
-            path: '../public/fonts/RolandTR808.woff2',
+            path: '../public/fonts/IBMPlexMono-Medium.ttf',
             weight: '400',
             style: 'normal'
         },
         {
-            path: '../public/fonts/RolandTR808.woff',
+            path: '../public/fonts/Inter-VariableFont.ttf',
             weight: '700',
-            style: 'normal'
+            style: 'bold'
         },
     ],
     variable: '--font-hail-bob',
